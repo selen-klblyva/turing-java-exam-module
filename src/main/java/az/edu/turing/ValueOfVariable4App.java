e0c9a8ba70e27a2ca5597b38f53044ffb3c89bf1
Selen <kalbaliyevaselen@gmail.com>
2024-02-02 16:45:35 +0400

    value of variable4

---
+++ src/main/java/az/edu/turing/ValueOfVariable4App.java
@@ -0,0 +1,10 @@
+package az.edu.turing;
+import java.util.Scanner;
+public class ValueOfVariable4App {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int x=scanner.nextInt();
+        float y=(float)(2*x-1/Math.pow(x,2)+Math.sqrt(x*x+1)/2);
+        System.out.printf("%.3f",y);
+    }
+}
