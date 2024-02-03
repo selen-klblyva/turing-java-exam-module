c06baa1f7b0e05dbecb38ebf0d80960ce1559b77
Selen <kalbaliyevaselen@gmail.com>
2024-02-03 22:54:11 +0400

    value of variable1

---
+++ src/main/java/az/edu/turing/ValueOfVariable1App.java
@@ -0,0 +1,11 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+public class ValueOfVariable1App {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int x=scanner.nextInt();
+        float y=(float)(Math.pow(x,3)-(5*Math.pow(x,2)/7)+9*x-(3/x)+1);
+        System.out.printf("%.3f",y);
+    }
+}
