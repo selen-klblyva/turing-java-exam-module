8f828a93be3bc159fa37369f95be898491323a23
Selen <kalbaliyevaselen@gmail.com>
2024-02-04 13:35:31 +0400

    the squareroot of numbers

---
+++ src/main/java/az/edu/turing/SquareRootApp.java
@@ -0,0 +1,16 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+
+public class SquareRootApp{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        double result = findSquareRoot(n);
+        System.out.printf("%.6f",result);
+    }
+    private static double findSquareRoot(long n) {
+        return Math.sqrt(n);
+    }
+}
